f00c0c8a433b5ce0b5a561fc0529845db944b93a
alanp <Alan.Pang@healthlink.net>
2018-11-15 11:09:28 +1300

    Flyway mistake

--- src/main/resources/db/migration/V1__create_initial_tables.sql
+++ src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -1,9 +1,9 @@
 CREATE TABLE user_account (
   user_id BIGSERIAL NOT NULL PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  username VARCHAR(20) NOT NULL,
+  email VARCHAR(25) NOT NULL,
   email_verified VARCHAR(10),
-  password VARCHAR(255) NOT NULL,
+  password VARCHAR(60) NOT NULL,
   password_salt VARCHAR(10) NOT NULL,
   session_active VARCHAR(10),
   verification_code VARCHAR(10)
